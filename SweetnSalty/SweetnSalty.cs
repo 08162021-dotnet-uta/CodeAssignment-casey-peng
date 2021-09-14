f4f938374f897f1b69affc22fe28658d8cb21627
13478 <13478@DESKTOP-CTAL6HT>
2021-09-14 12:38:58 -0400

    added

---
+++ SweetnSalty/SweetnSalty.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SweetAndSalty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countSweet = 0;
+            int countSalty = 0;
+            int countSweetNSalty = 0;
+           
+          
+                for(int i = 1; i <= 1000; i++)
+                {
+
+                    if(i%3 == 0 && i % 5 == 0)
+                    {
+                        Console.Write("sweet’nSalty ");
+                        countSweetNSalty++;
+                    }else if (i %3 == 0)
+                    {
+                        Console.Write("sweet ");
+                        countSweet++;
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.Write("salty ");
+                        countSalty++;
+                    }else 
+                {
+                    Console.Write(i);
+                }
+                    if(i%10==0)
+                {
+                    Console.WriteLine();
+                }
+            
+                }
+               
+            
+            Console.WriteLine($"TOTAL NUM OF SWEET AND SALTY{countSweetNSalty}");
+            Console.WriteLine($"TOTAL NUM OF SWEET {countSweet}");
+            Console.WriteLine($"TOTAL NUM OF  SALTY{countSalty}");
+        }
+    }
+}
